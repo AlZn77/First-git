a20c9d9ce87a206a0cf7547695c89fb3dad02e3e
13218 <13218@AlZn>
2020-09-07 20:15:09 +0800

    tset

---
+++ src/first_git/First_git.java
@@ -0,0 +1,10 @@
+package first_git;
+
+public class First_git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First git by@AlZn77");
+	}
+
+}
